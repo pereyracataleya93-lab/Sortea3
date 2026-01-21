120d98993a5ab4ec08377eb7dab6790870175439
pereyracataleya93-lab <pereyracataleya93@gmail.com>
2026-01-21 17:52:50 -0300

    config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  nombreSorteo: "Sortea3",
+  premio: "TV Smart 50\"",
+  precio: 1000,
+  telefonoAdmin: "549XXXXXXXXXX", // cambiar por el WhatsApp del organizador
+  aliasPago: "sortea3.alias"
+};
