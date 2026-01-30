1863a2d4b978fd9581e0556dbb5c2a03cd560ffc
pereyracataleya93-lab <pereyracataleya93@gmail.com>
2026-01-30 12:40:44 -0300

    config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   nombreSorteo: "Sortea3",
   premio: "Premio",
   precio: 1000,
-  telefonoAdmin: "5493512433338",
+  telefonoAdmin: "5493512438338",
   aliasPago: "sortea3.alias",
   adminPIN: "1234",
   totalNumeros: 20
